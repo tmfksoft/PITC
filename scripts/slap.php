5d09728a6672319dbda3688af965fb5effbc83c4
Thomas Edwards <tmfksoft@gmail.com>
2012-09-25 02:36:30 +0100

    General Edit to Slap Script

--- scripts/slap.php
+++ scripts/slap.php
@@ -1,8 +1,9 @@
-function load() {
-	global $api;
-	$api->addCommand("slap","slap_funct");
-}
+<?php
+global $api;
+$api->addCommand("slap","slap_funct");
+
 function slap_funct($irc) {
 	global $api;
 	$api->action($irc['active'],"slaps ".$irc['1']." with a PITC shaped trout");
 }
+?>
